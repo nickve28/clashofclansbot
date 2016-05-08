1d21a2a1d2ff2232c05eecba79818ce9365954c0
nickve28 <nickvaneyk@gmail.com>
2016-05-08 23:31:10 +0200

    Slight more elixir like refactors

--- lib/clash_of_clans_slackbot.ex
+++ lib/clash_of_clans_slackbot.ex
@@ -11,27 +11,26 @@ defmodule MessageParser do
   @empty_values ["", ", ", " "]
 
   def parse_response(message) do
-    [command | parameters] = String.split message, " ", parts: 2
-    case command do
-      "!startwar" -> parse_war(parameters)
-      _   -> { :no_content, nil }
-    end
+    [command, parameters] = String.split message, " ", parts: 2
+    parse_action command, parameters
   end
 
-  defp parse_war(parameters) do
-    params = Enum.at parameters, 0 #Str split sends it as array
-    [size | names ] = String.split params, " ", parts: 2
+  defp parse_action(_command="!startwar", parameters) do
+    [size | names ] = String.split parameters, " ", parts: 2
     parsed_size = String.to_integer size
     parsed_names = Enum.at(names, 0)
       |> String.split(~r/"/)
-      |> Enum.filter(&(!&1 in @empty_values))
+      |> Enum.reject(&(&1 in @empty_values))
       |> Enum.map(&(String.strip &1))
     name = Enum.at parsed_names, 0
     ename = Enum.at parsed_names, 1
     { :ok, req } = Clashcaller.Request.construct(name, ename, parsed_size)
     Clashcaller.Request.to_form_body(req)
       |> Clashcaller.start_war
+  end
 
+  defp parse_action(_command, _) do
+    { :no_content, _command }
   end
 
 end
