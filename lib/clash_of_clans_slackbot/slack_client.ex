b8ecf689f13f02fd6c848b61fe54a8b60be3c0e9
nickve28 <nickvaneyk@gmail.com>
2016-05-12 20:33:15 +0200

    lowercase for commands

--- lib/clash_of_clans_slackbot/slack_client.ex
+++ lib/clash_of_clans_slackbot/slack_client.ex
@@ -13,6 +13,7 @@ defmodule SlackClient do
 
   def handle_message(message = %{type: "message"}, slack, state) do
     text = message.text
+      |> String.downcase
     { status, response } = MessageParser.parse_response text
     if status === :ok do
       send_message(response, message.channel, slack)
