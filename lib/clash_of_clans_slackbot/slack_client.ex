fcbe9a610db8dfa3011cf089c820206f2f21b688
nickve28 <nickvaneyk@gmail.com>
2016-07-16 16:11:01 +0200

    Fixed the slack client API which changed majorly in the version..

--- lib/clash_of_clans_slackbot/slack_client.ex
+++ lib/clash_of_clans_slackbot/slack_client.ex
@@ -1,26 +1,28 @@
 defmodule SlackClient do
   use Slack
 
-  def handle_connect(slack, state) do
+  def handle_connect(slack) do
     IO.puts "Connected as #{slack.me.name}"
-    {:ok, state}
+    {:ok }
   end
 
-  def handle_message(message = %{hidden: true}, _slack, state) do #This is to prevent the hidden url messages from crashing everything
-    { :ok, state ++ [message.message.text] }
+  def handle_message(message = %{hidden: true}, slack) do #This is to prevent the hidden url messages from crashing everything
+    { :ok }
   end
 
-  def handle_message(message = %{type: "message"}, slack, state) do
+  def handle_message(message = %{type: "message"}, slack) do
     text = message.text
       |> String.downcase
     { status, response } = MessageParser.parse_response text
     if status === :ok do
       send_message(response, message.channel, slack)
     end
-    {:ok, state ++ [message.text]}
+    { :ok }
   end
 
-  def handle_message(_message, _slack, state) do
-    {:ok, state}
+  def handle_message(_message, slack) do
+    { :ok }
   end
+
+  def handle_info(_, _), do: :ok
 end
