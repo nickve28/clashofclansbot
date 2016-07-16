6813e3a60cb8c1a9dfe6d039b55468a9c9b5b25b
nickve28 <nickvaneyk@gmail.com>
2016-07-16 22:08:10 +0200

    Added empty dev and test which seem to be required (config)

---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :clash_of_clans_slackbot, token: "placeholder"
+config :clash_of_clans_slackbot, database: "dev.sqlite3"
+config :clash_of_clans_slackbot, clashapi_token: "sometoken"
+config :clash_of_clans_slackbot, post_channel: "bottesting"
+
