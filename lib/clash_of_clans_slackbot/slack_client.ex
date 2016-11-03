7f11716efe81233bf07d760a70cf2ac16eca5781
nickve28 <nickvaneyk@gmail.com>
2016-11-03 21:28:30 +0100

    Upgrade slack version to attempt to fix internet recon issues

--- lib/clash_of_clans_slackbot/slack_client.ex
+++ lib/clash_of_clans_slackbot/slack_client.ex
@@ -2,16 +2,16 @@ defmodule SlackClient do
   use Slack
   require Logger
 
-  def handle_connect(slack) do
+  def handle_connect(slack, state) do
     IO.puts "Connected as #{slack.me.name}"
-    {:ok }
+    {:ok, state}
   end
 
-  def handle_message(message = %{hidden: true}, slack) do #This is to prevent the hidden url messages from crashing everything
-    { :ok }
+  def handle_event(message = %{hidden: true}, slack, state) do #This is to prevent the hidden url messages from crashing everything
+    { :ok, state }
   end
 
-  def handle_message(message = %{type: "message"}, slack) do
+  def handle_event(message = %{type: "message"}, slack, state) do
     text = message.text
       |> String.downcase
 
@@ -20,27 +20,27 @@ defmodule SlackClient do
     if status === :ok do
       send_message(response, message.channel, slack)
     end
-    { :ok }
+    { :ok, state }
   end
 
-  def handle_message(message, _slack) do
+  def handle_event(message, _slack, state) do
     Logger.info("Received #{Poison.encode! message}")
-    { :ok }
+    { :ok, state }
   end
 
-  def handle_close(reason, socket_tuple) do
+  def handle_close({:error, :keepalive_timeout}, _slack, _state) do
+    exit {:error, :keepalive_timeout}
+  end
+
+  def handle_close(reason, slack, state) do
     Poison.encode!(reason)
       |> Logger.error
-    socket_tuple
-      |> Tuple.to_list
-      |> Enum.join(", ")
-      |> Logger.error
-    :ok
+    {:ok, state}
   end
 
-  def handle_info(message, _) do
+  def handle_info(message, _, state) do
     msg = Poison.encode!(message)
     Logger.error("handle_info received #{msg}")
-    :ok
+    { :ok, state }
   end
 end
--- mix.exs
+++ mix.exs
@@ -31,8 +31,7 @@ defmodule ClashOfClansSlackbot.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:slack, "~> 0.7.0"},
-     {:websocket_client, git: "https://github.com/jeremyong/websocket_client"},
+    [{:slack, "~> 0.8.0"},
      {:httpotion, "~> 2.2.0"},
      {:mock, "~> 0.1.3", only: :test},
      {:poison, "~> 2.0"},
