c8180767df5f201b8739583581a89953a2c96063
Nick van Eijk <nickve28@users.noreply.github.com>
2016-11-11 08:45:47 +0100

    Fixed all warnings in the project code (#51)

--- lib/clash_of_clans_slackbot/adapters/war_api.ex
+++ lib/clash_of_clans_slackbot/adapters/war_api.ex
@@ -2,6 +2,6 @@ defmodule ClashOfClansSlackbot.Behaviors.WarAPI do
   alias ClashOfClansSlackbot.Repositories.ClashCaller.ClashCallerEntry
   @callback start_war(Integer.t, String.t, String.t) :: Tuple.t
   @callback overview(String.t) :: [%ClashCallerEntry{}]
-  @callback reserve_attack(Integer.t String.t, String.t) :: String.t
+  @callback reserve_attack(Integer.t, String.t, String.t) :: String.t
   @callback register_attack(String.t, Integer.t, Integer.t, Integer.t) :: String.t
 end
