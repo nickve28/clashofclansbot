7df29fefa3da898eaa23af921f9f1394ea493496
nickve28 <nickvaneyk@gmail.com>
2016-11-16 21:24:24 +0100

    v1.4.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ClashOfClansSlackbot.Mixfile do
 
   def project do
     [app: :clash_of_clans_slackbot,
-     version: "1.4.0",
+     version: "1.4.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
